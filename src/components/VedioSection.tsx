5e3412c3ee26eaf96dd58ff420d28db3df56f2ce
Zobex <zobex.game@gmail.com>
2025-04-16 16:25:41 +0500

    fix:video issues

--- src/components/VedioSection.tsx
+++ src/components/VedioSection.tsx
@@ -2,17 +2,13 @@ import React from "react";
 
 function VedioSection() {
   return (
-    <div className="w-full bg-transparent">
-      <div className="vedioBox">
-        <video
-          src="/assets/video.mp4"
-          className="w-full titleInfo"
-          autoPlay
-          loop
-          muted
-        />
-      </div>
-    </div>
+    <video
+      src="assets/video.mp4"
+      className="w-full titleInfo"
+      autoPlay
+      loop
+      muted
+    />
   );
 }
 
