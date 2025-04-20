0501a92220bbea5318f99187645d499c91ff85e6
Zobex <zobex.game@gmail.com>
2025-04-20 13:35:42 +0500

    fix:typo

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function VideoSection() {
   return (
     <video
-      src="/assets/video.mp4"
+      src="/assets/vedio.mp4"
       className="w-full titleInfo"
       autoPlay
       loop
