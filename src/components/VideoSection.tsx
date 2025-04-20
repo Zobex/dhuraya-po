767e5ddb26d5d95d223e17a346d997394aadd9a3
Zobex <zobex.game@gmail.com>
2025-04-20 13:46:50 +0500

    fix:issue

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,8 +1,17 @@
+"use client";
 import React from "react";
 
 function VideoSection() {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  React.useEffect(() => {
+    videoRef.current?.play().catch((err) => {
+      console.warn("Autoplay failed", err);
+    });
+  }, []);
   return (
     <video
+      ref={videoRef}
       src="/assets/vedio.mp4"
       className="w-full titleInfo"
       autoPlay
